63e0db2ae14f129a8573328eb4231cef2fa5b5fd
OjashviSingh <ojashvisingh365@gmail.com>
2023-01-22 14:46:32 +0530

    Added: Power of 2 problem

---
+++ PowerOf2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Random;
+public class PowerOf2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year to check:");
+        int userNumber = sc.nextInt();
+
+    }
+}
